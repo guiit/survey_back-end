9a2bb2c07f49b465d3d97f36741bbf959445e778
Guilherme <luiz.guilherme@sambatech.com.br>
2021-03-05 21:23:11 -0300

    refactor: ensure that the signup is typed

--- src/presentation/controller/signup.spec.ts
+++ src/presentation/controller/signup.spec.ts
@@ -1,5 +1,4 @@
 import { SignupController } from './signup';
-
 describe('Signup Controller', () => {
   test('should return 400 if no name is provided', () => {
     const sut = new SignupController();
@@ -11,6 +10,7 @@ describe('Signup Controller', () => {
       }
     };
     const httpResponse = sut.handle(httpRequest);
+
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new Error('Missing param: name'));
   });
