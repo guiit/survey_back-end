6dde6d0aa89bc5d0647f5199df066beeca456234
Guilherme <luiz.guilherme@sambatech.com.br>
2021-03-06 21:37:57 -0300

    test: adding validation to check if password confirmation is provided

--- src/presentation/controller/signup.spec.ts
+++ src/presentation/controller/signup.spec.ts
@@ -42,4 +42,19 @@ describe('Signup Controller', () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError('password'));
   });
+  test('should return 400 if no passwordConfirmation is provided', () => {
+    const sut = new SignupController();
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@email',
+        password: '123'
+      }
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(
+      new MissingParamError('passwordConfirmation')
+    );
+  });
 });
