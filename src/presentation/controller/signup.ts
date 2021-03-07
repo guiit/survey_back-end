6ef16b3aba1f01fd52be2db23e282a0cbe167463
Guilherme <luiz.guilherme@sambatech.com.br>
2021-03-06 21:37:34 -0300

    feat: adding validation to check if password confirmation is provided

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -4,7 +4,12 @@ import { MissingParamError } from '../errors/missing-param-error';
 
 export class SignupController {
   handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email', 'password'];
+    const requiredFields = [
+      'name',
+      'email',
+      'password',
+      'passwordConfirmation'
+    ];
     for (const field of requiredFields)
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field));
