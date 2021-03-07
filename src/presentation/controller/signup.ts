34c8e7353ebdd7b519d66c3efe2ec64eded4900c
Guilherme <luiz.guilherme@sambatech.com.br>
2021-03-06 21:25:24 -0300

    refactor: adding missing param error class to the controller

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -1,12 +1,13 @@
 import { HttpRequest, HttpResponse } from '../protocols/http';
 import { badRequest } from '../helpers/http-helper';
+import { MissingParamError } from '../errors/missing-param-error';
 
 export class SignupController {
   handle(httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email'];
     for (const field of requiredFields)
       if (!httpRequest.body[field]) {
-        return badRequest(new Error(`Missing param: ${field}`));
+        return badRequest(new MissingParamError(`Missing param: ${field}`));
       }
 
     return { statusCode: 200, body: 'Ok' };
