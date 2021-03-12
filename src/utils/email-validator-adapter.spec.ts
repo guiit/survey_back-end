860b0124473e478e9ec14862853f64b8f8d6cb99
Guilherme <luiz.guilherme@sambatech.com.br>
2021-03-12 07:55:34 -0300

    test: creation of factory for email validator spec

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,4 +1,4 @@
-import { EmailValidatorAdapter } from './email-validator';
+import { EmailValidatorAdapter } from './email-validator-adapter';
 import validator from 'validator';
 
 jest.mock('validator', () => ({
@@ -7,16 +7,27 @@ jest.mock('validator', () => ({
   }
 }));
 
+const makeSut = () => {
+  return new EmailValidatorAdapter();
+};
+
 describe('EmailValidator Adapter', () => {
   test('Should return false if email is invalid', () => {
-    const sut = new EmailValidatorAdapter();
+    const sut = makeSut();
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false);
     const isValid = sut.isValid('invalid_emai');
     expect(isValid).toBe(false);
   });
   test('Should return true if email is valid', () => {
-    const sut = new EmailValidatorAdapter();
+    const sut = makeSut();
     const isValid = sut.isValid('valid_email@mail.com');
     expect(isValid).toBe(true);
+  });
+  test('Should call valid method with correct param', () => {
+    const sut = makeSut();
+    const isEmailSpy = jest.spyOn(validator, 'isEmail');
+    const email = 'valid_email@mail.com';
+    sut.isValid(email);
+    expect(isEmailSpy).toHaveBeenCalledWith(email);
   });
 });
