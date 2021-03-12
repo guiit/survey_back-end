49b15218af31ff75755e4642dfddc9c3bd715462
Guilherme <luiz.guilherme@sambatech.com.br>
2021-03-12 07:45:30 -0300

    feat: validator email

---
+++ src/utils/email-validator.ts
@@ -0,0 +1,10 @@
+import { EmailValidator } from '../presentation/protocols/email-validator';
+import validator from 'validator';
+
+export class EmailValidatorAdapter implements EmailValidator {
+  isValid(email: string): boolean {
+    const result = validator.isEmail(email);
+
+    return result;
+  }
+}
