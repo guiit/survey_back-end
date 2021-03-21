38b793ed2c03055eb649ab68e2389df56654c38c
Guilherme <luiz.guilherme@sambatech.com.br>
2021-03-21 12:32:16 -0300

    feat: express server

---
+++ src/main/config/env.ts
@@ -0,0 +1,5 @@
+export default {
+  mongoUrl:
+    process.env.MONGO_URL || 'mongodb://localhost:27017/survey_back-end',
+  port: process.env.PORT || 5050
+};
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,4 +1,13 @@
-import app from './config/app';
-const PORT = 5050;
+/* eslint-disable no-console */
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper';
+import env from './config/env';
 
-app.listen(PORT, () => console.log('Server listening on PORT', PORT));
+MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    const app = (await import('./config/app')).default;
+
+    app.listen(env.port, () =>
+      console.log('Server listening on PORT', env.port)
+    );
+  })
+  .catch(console.error);
